fec78550ec0744d4327d8a7d4d75f6e29b9cac8b
Bin Wang <bin.wang@mail.binwang.me>
2025-09-19 21:34:50 -0400

    move file to model mod

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-mod profile;
 
 use clap::{Parser, Subcommand};
+
+mod model;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -20,7 +21,7 @@ fn main() {
     let cli = Cli::parse();
     match cli.command {
         Commands::CreateProfile { path } => {
-            profile::create_template_file(&path).unwrap();
+            model::profile::create_template_file(&path).unwrap();
             println!("Profile template file created successfully at {}", &path);
         }
     }
